b11ba34c66e8ac5898632f1167a68460a105934f
groom115 <shyamabhishek115@gmail.com>
2025-01-11 22:20:38 -0500

    Login view intregated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
+import LoginPage from './views/login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Provider >
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/dashboard" />} /> */}
+      </Routes>
+    </Router>
+    // </Provider>
   );
 }
 
